8ea744da89bed129eae657e269ea0e1401fae042
Bruno Groppo <bmgroppo@gmail.com>
2025-08-25 13:33:53 -0300

    criado interface

---
+++ src/app/models/policial.model.ts
@@ -0,0 +1,14 @@
+export interface Policial{
+    id?:number;
+    rg_civil:string;
+    rg_militar:string;
+    cpf:string;
+    data_nascimento:Date;
+    
+    
+    // //"rg_civil": "12.345.678-9",
+	// 	"rg_militar": "12345",
+	// 	"cpf": "111.222.333-44",
+	// 	"data_nascimento": "1985-05-10T03:00:00.000Z",
+	// 	"matricula":
+}
