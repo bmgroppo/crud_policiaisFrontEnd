082cb74058f71dea1fa3d640874d3959fec23b74
Bruno Groppo <bmgroppo@gmail.com>
2025-08-25 14:08:14 -0300

    criado form policial TS

--- src/app/components/policial-form/policial-form.component.ts
+++ src/app/components/policial-form/policial-form.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { PoliciaisService } from '../../services/policiais.service';
 
 @Component({
   selector: 'app-policial-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './policial-form.component.html',
   styleUrl: './policial-form.component.css'
 })
-export class PolicialFormComponent {
+export class PolicialFormComponent implements OnInit {
+  policialForm: FormGroup;
+  isEdit: boolean = false;
+  policialId: number | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private policiaisService: PoliciaisService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.policialForm = this.fb.group({
+      nome: ['', Validators.required],
+      RG_civil: ['', Validators.required],
+      RG_militar: ['', Validators.required],
+      cpf: ['', Validators.required],
+      dataNascimento: ['', Validators.required],
+      matricula:['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      this.policialId = +idParam;
+      this.isEdit = true;
+      this.policiaisService.getPoliciaisById(this.policialId).subscribe(policial => {
+        this.policialForm.patchValue(policial);
+      });
+    }
+  }
+  onSubmit(): void {
+    if (this.policialForm.invalid) {
+      return;
+    }
+    const policialData = this.policialForm.value;
+    if (this.isEdit && this.policialId) {
+      this.policiaisService.updatePolicial(this.policialId, policialData).subscribe(() => {
+        this.router.navigate(['/policiais']);
+      });
+    } else {
+      this.policiaisService.createPolicial(policialData).subscribe(() => {
+        this.router.navigate(['/policiais']);
+      });
+    }
+  }
 }
