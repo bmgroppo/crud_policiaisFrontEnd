7933c76fcbd8beaa4163a58cf2837467ddcb0a22
Bruno Groppo <bmgroppo@gmail.com>
2025-08-25 14:19:16 -0300

    criado as rotas no app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PolicialListComponent } from './components/policial-list/policial-list.component';
+import { PolicialFormComponent } from './components/policial-form/policial-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: PolicialListComponent},
+    {path: 'policiais', component: PolicialListComponent},
+    {path: 'policiais/novo', component: PolicialFormComponent},
+    {path: 'policiais/:id', component: PolicialFormComponent}
+];
